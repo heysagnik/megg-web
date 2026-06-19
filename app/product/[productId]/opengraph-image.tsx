1e5db578118ac577eebde11c639b85bac11c4c20
Sagnik Sahoo <sahoosagnik1@gmail.com>
2026-06-19 22:18:23 +0530

    feat: add dynamic OpenGraph image generation for product pages

--- app/product/[productId]/opengraph-image.tsx
+++ app/product/[productId]/opengraph-image.tsx
@@ -23,12 +23,12 @@ export default async function Image({ params }: Props) {
 
   return new ImageResponse(
     (
-      <div style={{ width: '100%', height: '100%', display: 'flex', background: '#ffffff' }}>
+      <div style={{ width: '100%', height: '100%', display: 'flex', background: '#F3F3F3' }}>
         {imgSrc ? (
           // eslint-disable-next-line @next/next/no-img-element
           <img src={imgSrc} alt={product?.name ?? ''} style={{ width: '100%', height: '100%', objectFit: 'contain' }} />
         ) : (
-          <div style={{ color: '#F3F3F3', fontSize: 64, margin: 'auto', letterSpacing: '-2px' }}>MEGG</div>
+          <div style={{ color: '#fff', fontSize: 64, margin: 'auto', letterSpacing: '-2px' }}>MEGG</div>
         )}
       </div>
     ),
